de729f6bc138b6b8818b855f769329ae073933ec
Evoi95 <evoi95@gmail.com>
2019-06-29 23:56:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/textserch/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.textserch;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ExampleAdapter adapter;
+    private List<ExampleItem> exampleList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fillExampleList();
+        setUpRecyclerView();
+    }
+
+    private void fillExampleList() {
+        exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "One", "Ten"));
+        exampleList.add(new ExampleItem(R.drawable.ic_battery_std, "Two", "Eleven"));
+        exampleList.add(new ExampleItem(R.drawable.ic_folder, "Three", "Twelve"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Four", "Thirteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_battery_std, "Five", "Fourteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_folder, "Six", "Fifteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Seven", "Sixteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_battery_std, "Eight", "Seventeen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_folder, "Nine", "Eighteen"));
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        adapter = new ExampleAdapter(exampleList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    // messa io
+    //Fa riverimento alla cartella in res menù c'è dentro un file
+    // a cui faccio riferimento qua
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+
+    }
+}
